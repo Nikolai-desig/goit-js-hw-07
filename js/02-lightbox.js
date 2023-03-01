02b8f05c4f87f0079cb9e13882e25d04d07baa4c
Nikolai-desig <rinzleramad79@gmail.com>
2023-03-01 13:15:23 +0200

    допрацювання

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,22 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const gallery = document.querySelector('.gallery');
+const items = galleryItems.map(item => {
+    return `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>`}).join("");
 
-gallery.innerHTML = galleryItems
-    .map(({ preview, original, description }) => {
-        return `
-        <li>
-        <a class="gallery__items href="${original}">
-            <img class="gallery__image" src="${preview}" alt="${description}" />
-        </a>
-        </li>
-        `
-    }
-).join('');
-    
-let lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
-});
+const galleryBox = document.querySelector('.gallery');
+galleryBox.innerHTML = items;
+
+
+let lightbox = new SimpleLightbox('.gallery a', { caption: true, captionSelector: 'img[alt]', captionType: 'attr', captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250 }); 
+
+
 console.log(galleryItems);
